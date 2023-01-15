71ac82eebd3a2ef9e02282328775f4cc36790e93
Justin Simon <justinsimon@Justins-MBP.lan>
2023-01-14 20:31:00 -0800

    Completed methods excercise pt 1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MethodsExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+
+            Console.WriteLine("What is your favorite planet?");
+            var planet = Console.ReadLine();
+
+
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+
+            Console.WriteLine("What is your favorite video game?");
+            var videogame = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name} we are sending you to {planet} to recover a {animal} that accidently got launched while the astronauts were busy playing {videogame}!");
+            
+        }
+    }
+}
